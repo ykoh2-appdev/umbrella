a83e8707dc1549524121f47c16f68dc402139ab0
Yoon Koh <ykoh2@chicagobooth.edu>
2023-04-06 14:50:39 +0000

    long & lat

--- umbrella.rb
+++ umbrella.rb
@@ -3,12 +3,24 @@ p "Where are you located?"
 # user_location = gets.chomp
 user_location = "Chicago"
 
-gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{user_location}&key=AIzaSyB92cYxPcYqgjwBJfWlwDQw_7yjuyU3tpA"
-
-# p gmaps_url
-
+gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{user_location}&key=#{ENV.fetch("GMAPS_KEY")}"
 
 require "open-uri"
 raw_response = URI.open(gmaps_url).read
 
-p raw_response.length
+require "json"
+parsed_response = JSON.parse("raw_response")
+
+results_array = parsed_response.fetch("results")
+
+first_result = results_array.at(0)
+
+geo = first_result.fetch("geometry")
+
+loc = geo.fetch("location")
+
+latitude = loc. fetch("lat")
+longitude = loc.fetch("lng")
+
+p latitude
+p longitude
